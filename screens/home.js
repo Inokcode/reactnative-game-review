4f545615e05c4f639598d7d99e375307305d6c87
Inokcode <regilearn@gmail.com>
2021-08-25 14:24:32 +0530

    first commit

---
+++ screens/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Home() {
+  return <View style={StyleSheet.container}>GBU</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
