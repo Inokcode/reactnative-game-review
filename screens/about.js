e387a9597a9418f26d07f6287f51da5fa3a37051
Inokcode <regilearn@gmail.com>
2021-08-25 17:49:51 +0530

    custom font added

---
+++ screens/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function About() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
--- screens/home.js
+++ screens/home.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 export default function Home() {
-  return <View style={StyleSheet.container}>GBU</View>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>GBU all</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     padding: 24,
+  },
+  titleText: {
+    fontFamily: 'nunito-bold',
+    fontSize: 18,
   },
 });
