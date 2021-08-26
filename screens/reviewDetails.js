7ef0baf9f9407f5960b80eebb6879852009963bc
Inokcode <regilearn@gmail.com>
2021-08-26 17:21:39 +0530

    modal added

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, View, Text, Image, ImageBackground } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({ navigation, route }) {
   const { title, body, rating } = route.params;
   return (
-    <View style={globalStyles.container}>
+    <ImageBackground
+      source={require('../assets/game_bg.png')}
+      style={globalStyles.container}
+    >
       <Card>
         <Text>{title}</Text>
         <Text>{body}</Text>
-        <Text>{rating}</Text>
+
+        <View style={styles.rating}>
+          <Text>GameZone rating:</Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
-    </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  },
+});
